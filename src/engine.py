638d78b7b49200d31d5794c9612f96859ba853b3
Naseela Pervez <perveznaseela@gmail.com>
2022-06-09 10:04:51 +0530

    engine object created

--- src/engine.py
+++ src/engine.py
@@ -1,20 +1,44 @@
-from .scraping import Scraper
+import json
+import os
 from tqdm import tqdm
+from .scraping import Scraper
 from .preprocessing import Preprocessor
+from .models import SentimentAnalyser
 
-##* 2.
-##* Collect 10 most recent articles from https://www.aljazeera.com/where/mozambique/
-##* Include collected articles as a JSON file in your submission repository.
-scraper = Scraper("https://www.aljazeera.com/where/mozambique/",
-                tag="article",
-                )
-scraper.save("data/aljazeera.json")
 
-##* 3.
-##* Pre-process the data. Remove anything that is not part of the article itself, e.g. comments,
-##* publishing date, images, etc. Make sure the articles are in English and can be processed by
-##* the sentiment analysis library.
-preprocessor = Preprocessor("data/aljazeera.json")
-data = preprocessor.preprocess()
+class Engine:
+    def __init__(self, url: str, save_path: str):
+        """_summary_
 
-print(data)
+        Args:
+            url (str): url of the page to scrape.
+        """
+        self.url = url
+        self.save_path = save_path
+        self.scraper = Scraper(url, tag="article")
+        print("downloading data...")
+        self.scraper.save(save_path)
+        self.preprocessor = Preprocessor(save_path)
+        self.analyser = SentimentAnalyser()
+
+    def process(self):
+        data = self.preprocessor.preprocess()
+        print("analysing data...")
+        results = {}
+        for item in tqdm(data):
+            title = item["title"]
+            text = self.preprocessor.only_english(item["text"])
+            sentiment = self.analyser.analyse(text)
+            results[title] = sentiment
+
+        with open(os.path.join(os.path.dirname(self.save_path), "results.json"), "w") as f:
+            json.dump(results, f, indent=4)
+
+        print("Results saved to", os.path.join(
+            os.path.dirname(self.save_path), "results.json"))
+
+
+if __name__ == "__main__":
+    engine = Engine("https://www.aljazeera.com/where/mozambique/",
+                    "data/aljazeera.json")
+    engine.process()
